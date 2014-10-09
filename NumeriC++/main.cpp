c1b0f46c23451ea9b49d2c72a418d6d76330f05b
Thierry BM <beggars.banquet@gmail.com>
2014-10-08 21:39:12 -0400

    Initial Commit

---
+++ NumeriC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  NumeriC++
+//
+//  Created by Thierry BM on 2014-10-08.
+//  Copyright (c) 2014 Thierry BM. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
